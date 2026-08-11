5bc862d6444c302103190edcaee615dcfc583792
Piyush Jain <Piyushjain1857@gmail.com>
2026-08-11 16:00:19 +0530

    added expro

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,7 +1,18 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 process.env.EXPO_ROUTER_APP_ROOT = './app';
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
